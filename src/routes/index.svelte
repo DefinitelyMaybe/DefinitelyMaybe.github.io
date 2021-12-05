7c0a7b262d76ebe8ddf0d7e3e8036f25a0a85fac
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-06 11:50:43 +1300

    github actions init

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,50 +8,35 @@
 
 <svelte:head>
 	<title>Aaron Dekker - Developer</title>
-	<meta property="og:title" content="Aaron Dekker - DefinitelyMaybe - Developer">
-	<meta property="og:image" content="https://definitelymaybe.github.io/images/lead-bg.jpg">
-	<meta property="og:description" content="Code Wrangler | Developer | Mades it work">
-	<meta property="og:url" content="https://definitelymaybe.github.io/">
+	<meta property="og:title" content="Aaron Dekker - DefinitelyMaybe - Developer" />
+	<meta property="og:image" content="https://definitelymaybe.github.io/images/lead-bg.jpg" />
+	<meta property="og:description" content="Code Wrangler | Developer | Mades it work" />
+	<meta property="og:url" content="https://definitelymaybe.github.io/" />
 </svelte:head>
 
 <section>
-	<h1>
-		Aaron Dekker
-	</h1>
+	<h1>Aaron Dekker</h1>
 	<h2>
-		Header page <br>
-		Code Wrangler | BSc Graduate | Seeker of the Longest Path <br>
-		? interests -> three.js, Deno, svelte, svelte.kit, svelte-cubed
-		? about section
+		Header page <br />
+		Code Wrangler | BSc Graduate | Seeker of the Longest Path <br />
+		? interests -> three.js, Deno, svelte, svelte.kit, svelte-cubed ? about section
 	</h2>
 
-	<h2>
-		- A grid of Projects and things I'd like to show off
-	</h2>
-	<h2>
-		- A light-dark theme switch (use of cookies and local storage)
-	</h2>
-	<h2>
-		- A project about how I made the website
-	</h2>
-	<h2>
-		- Use defer scripts if needed
-	</h2>
+	<h2>- A grid of Projects and things I'd like to show off</h2>
+	<h2>- A light-dark theme switch (use of cookies and local storage)</h2>
+	<h2>- A project about how I made the website</h2>
+	<h2>- Use defer scripts if needed</h2>
 	<h2>
 		- Try out <a href:external href="https://www.npmjs.com/package/seolint">seolint</a>
 	</h2>
+	<h2>- Do a small svelte-cubed project</h2>
 	<h2>
-		- Do a small svelte-cubed project
+		- Try out some nice <a href:external href="https://www.youtube.com/watch?v=lPJVi797Uy0"
+			>curves</a
+		>
 	</h2>
-	<h2>
-		- Try out some nice <a href:external href="https://www.youtube.com/watch?v=lPJVi797Uy0">curves</a>
-	</h2>
-	<h2>
-		- Image sizing for different viewports
-	</h2>
-	<h2>
-		- Show off an API
-	</h2>
+	<h2>- Image sizing for different viewports</h2>
+	<h2>- Show off an API</h2>
 	<Counter />
 </section>
 
