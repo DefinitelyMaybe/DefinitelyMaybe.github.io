467c069b21ec276830d491a0ba452636190c47d6
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-08 16:11:19 +1300

    add box component

---
+++ src/lib/Box.svelte
@@ -0,0 +1,18 @@
+<script>
+  /** @type {string} */
+  export let src
+  /** @type {string} */
+  let alt // export
+  let shape // export
+</script>
+
+<div class="{shape}">
+  <img {src} {alt}>
+  <slot name="text"></slot>
+</div>
+
+<!-- <style>
+  div {
+    @apply text-base;
+  }
+</style> -->
