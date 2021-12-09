4fad0f32f51c917e23f141facfa953a676eb5a90
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-10 12:21:22 +1300

    made writable theme

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,75 +1,81 @@
+<script context="module">
+	import { writable } from 'svelte/store';
+	export const theme = writable();
+</script>
+
 <script>
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import Toggle from '$lib/Toggle.svelte';
 	import ghLogo from '$lib/assets/images/GitHub-Mark-32px.png';
 	import ghLogoLight from '$lib/assets/images/GitHub-Mark-Light-32px.png';
-	// import svLogo from "./svelte-logo.svg";
-	// import ghLogo from 'static/images/GitHub-Mark-32px.png';
-	export let theme;
+
+	let ghLogoSrc = ghLogo;
+
+	theme.subscribe((val) => {
+		console.log(val);
+		ghLogoSrc = ghLogo;
+		if (val == 'dark') {
+			ghLogoSrc = ghLogoLight;
+		}
+	});
+
+	$: if ($theme) {
+		document.body.className = $theme;
+	}
 
 	function toggleTheme() {
-		if (theme === 'light') {
-			theme = 'dark';
+		if ($theme === 'light') {
+			theme.set('dark');
 			localStorage.setItem('theme', 'dark');
 		} else {
-			theme = 'light';
+			theme.set('light');
 			localStorage.setItem('theme', 'light');
 		}
-		document.body.className = theme;
 	}
 
 	onMount(() => {
 		// If no localStorage.theme has been set then set one
 		if (localStorage.getItem('theme') == undefined) {
 			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-				theme = 'dark';
+				theme.set('dark');
 				localStorage.setItem('theme', 'dark');
 			} else {
 				localStorage.setItem('theme', 'light');
 			}
 		} else {
-			theme = localStorage.getItem('theme');
+			theme.set(localStorage.getItem('theme'));
 		}
-		document.body.className = theme;
 	});
 </script>
 
 <header
-	class="flex flex-row justify-between px-2 pb-2
- dark:bg-black dark:text-white"
+	class="flex flex-col bg-[#f1f3f5] border-b-[1px]
+	dark:bg-[#2d333b] dark:border-gray-800"
 >
-	<div>
-		<label for="">dark</label>
-		<Toggle checked={theme === 'dark'} on:click={toggleTheme} />
-	</div>
+	<h1 class="text-center font-bold">Aaron Dekker</h1>
+	<div class="flex flex-row justify-between px-2 pb-2">
+		<div>
+			<label for="">dark</label>
+			<Toggle checked={$theme === 'dark'} on:click={toggleTheme} />
+		</div>
 
-	<nav>
-		<ul class="flex flex-row space-x-5 text-2xl font-bold">
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Projects</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-		</ul>
-	</nav>
+		<nav>
+			<ul class="flex flex-row space-x-5 text-2xl font-bold">
+				<li class:active={$page.path === '/'} class="text-gray-400">
+					<a sveltekit:prefetch href="/">Projects</a>
+				</li>
+				<li>|</li>
+				<li class:active={$page.path === '/about'}>
+					<a sveltekit:prefetch href="/about">About</a>
+				</li>
+			</ul>
+		</nav>
 
-	<div class="corner">
-		{#if theme === 'light'}
+		<div class="corner">
 			<a href="https://github.com/DefinitelyMaybe">
-				<img src={ghLogo} alt="Github Icon" />
+				<img src={ghLogoSrc} alt="Github Icon" />
 			</a>
-		{:else}
-			<a href="https://github.com/DefinitelyMaybe">
-				<img src={ghLogoLight} alt="Github Icon" />
-			</a>
-		{/if}
+		</div>
 	</div>
 </header>
-
-<style>
-	ul {
-		@apply px-0;
-	}
-
-	li {
-		@apply px-2;
-	}
-</style>
