0c2498e2e83da62650f22a616c11f077646b4aa6
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-09 16:22:13 +1300

    remove css classes for the moment

--- src/lib/Toggle.svelte
+++ src/lib/Toggle.svelte
@@ -6,6 +6,7 @@
 	type="checkbox"
 	{checked}
 	on:click
-	class="appearance-none bg-white border-2 border-black w-4 h-4 rounded-md
-  dark:border-white dark:bg-black"
 />
+
+<!-- class="appearance-none bg-white border-2 border-black w-4 h-4 rounded-md
+  dark:border-white dark:bg-black" -->
