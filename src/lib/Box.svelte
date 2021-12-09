45de174be476e553ba3ea4efbcf4091cba163522
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-10 12:20:59 +1300

    added shadow

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -2,17 +2,14 @@
 	/** @type {string} */
 	export let src;
 	/** @type {string} */
-	let alt; // export
+	export let alt; // export
 	let shape; // export
 </script>
 
-<div class="{shape} rounded-md text-center">
+<div
+	class="{shape} rounded-md text-center shadow-lg border-[1px] pb-1
+	dark:shadow-gray-900 dark:shadow-md dark:border-[1px] dark:border-gray-900"
+>
 	<img {src} {alt} class="w-full rounded-md" />
 	<slot />
 </div>
-
-<!-- <style>
-  div {
-    @apply text-base;
-  }
-</style> -->
