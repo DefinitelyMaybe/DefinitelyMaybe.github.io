196e4118ce3102db8494d3a3353ab33b7d48607f
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-09 14:00:25 +1300

    text slot and tailwindcss

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -1,14 +1,14 @@
 <script>
-  /** @type {string} */
-  export let src
-  /** @type {string} */
-  let alt // export
-  let shape // export
+	/** @type {string} */
+	export let src;
+	/** @type {string} */
+	let alt; // export
+	let shape; // export
 </script>
 
-<div class="{shape}">
-  <img {src} {alt}>
-  <slot name="text"></slot>
+<div class="{shape} bg-gray-800 rounded-md">
+	<img {src} {alt} />
+	<slot name="text" />
 </div>
 
 <!-- <style>
