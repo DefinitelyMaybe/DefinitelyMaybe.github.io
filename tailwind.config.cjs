a4315e21ab6a8cb8ca5fe1e6c8f5d16f1d5855b4
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-10 12:18:06 +1300

    update tailwindcss to 3.0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,13 @@
 
 module.exports = {
 	mode: 'jit',
-	purge: ['./src/**/*.svelte', './src/**/*.css'],
+	content: ['./src/**/*.svelte', './src/**/*.css'],
 	darkMode: 'class',
-	// theme: {
-	//   colors: {
-	//     white: colors.white,
-	//     gray: colors.warmGray,
-	//     'svelte-prime': '#ff5030',
-	//   },
-	// },
 	variants: {
 		extend: {}
 	},
+	// preset: [
+	// 	['./src/**/*.svelte', './src/**/*.css']
+	// ],
 	plugins: []
 };
