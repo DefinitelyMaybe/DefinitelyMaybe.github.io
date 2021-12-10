31c4f9fd35a17cb8028dd6d416c88f1a5261b863
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-10 17:07:06 +1300

    working draft of toggle

--- src/lib/Toggle.svelte
+++ src/lib/Toggle.svelte
@@ -2,7 +2,14 @@
 	export let checked = false;
 </script>
 
-<input type="checkbox" {checked} on:click />
-
-<!-- class="appearance-none bg-white border-2 border-black w-4 h-4 rounded-md
-  dark:border-white dark:bg-black" -->
+<div
+	class="flex bg-white border-2 border-black w-6 h-4 rounded-lg
+  dark:bg-black dark:border-white dark:flex-row-reverse"
+	on:click
+>
+	<input type="checkbox" {checked} class="appearance-none" />
+	<div
+		class="w-3 h-3 bg-black rounded-lg
+    dark:bg-white"
+	/>
+</div>
