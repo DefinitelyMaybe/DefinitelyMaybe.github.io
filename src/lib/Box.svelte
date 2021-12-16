c5ad024db44a6e8e48070c7256e7900cebc056bf
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-16 15:46:10 +1300

    used name slots and flex

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -3,15 +3,17 @@
 	export let src;
 	/** @type {string} */
 	export let alt;
-	let shape;
 </script>
 
 <div
-	class="{shape} rounded-md text-center shadow-lg border-[1px] pb-1
+	class="flex flex-col justify-start rounded-md text-center shadow-lg border-[1px] pb-1
 	dark:shadow-[#2d333b] dark:shadow-md dark:border-[1px] dark:border-[#2d333b]"
 >
-	<img {src} {alt} class="w-full rounded-md" />
-	<div class="px-2 pb-1">
-		<slot />
+	<img {src} {alt} class="rounded-md w-full" />
+	<div class="px-2 py-2">
+		<slot name="desc" />
+	</div>
+	<div class="px-2 py-2">
+		<slot name="link" />
 	</div>
 </div>
