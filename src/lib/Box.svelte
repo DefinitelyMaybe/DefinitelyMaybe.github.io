b9d9e6c8255d5d2eaac12cb3956e212eb87c5f86
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-17 15:09:01 +1300

    navigate on click. not just on anchor element

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -1,13 +1,16 @@
 <script>
+	import { goto } from "$app/navigation";
 	/** @type {string} */
 	export let src;
 	/** @type {string} */
 	export let alt;
+	export let href;
 </script>
 
-<div
+<div on:click="{goto(href)}"
 	class="flex flex-col justify-start rounded-md text-center shadow-lg border-[1px] pb-1
-	dark:shadow-[#2d333b] dark:shadow-md dark:border-[1px] dark:border-[#2d333b]"
+	dark:shadow-[#2d333b] dark:shadow-md dark:border-[1px] dark:border-[#2d333b]
+		hover:cursor-pointer"
 >
 	<img {src} {alt} class="rounded-md w-full" />
 	<div class="px-2 py-2">
