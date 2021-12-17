ae09711e21f01375e0c8adcfdb428e48bd6627bc
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-17 15:12:34 +1300

    reorder, reword and new images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,8 +10,6 @@
 	import threejsdarkImg from '$lib/assets/images/threejslogodark.png';
 	import threejswhiteImg from '$lib/assets/images/threejslogowhite.png';
 	import lavaCubeImg from '$lib/assets/images/cubes.png';
-	import revealjsImg from '$lib/assets/images/revealjs.png';
-	import mathsImg from '$lib/assets/images/mathsplayground.png';
 	import tugImg from '$lib/assets/images/TUG/TUGconceptArtmini.jpg';
 
 	let threeSrc = threejswhiteImg;
@@ -30,17 +28,16 @@
 </svelte:head>
 
 <ul class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4">
-	<Box src={fallingImg} alt="Falling 0's and 1's">
+	<Box src={fallingImg} alt="Falling 0's and 1's" href="/projects/remake/">
 		<div slot="desc">
-			<h2 class="font-bold">Remaking the Portfolio</h2>
-			<p>A quick story about remaking this entire website.</p>
-			<p>Side note: It's not finished.</p>
+			<h2 class="font-bold">Remixing the Old</h2>
+			<p>A story about remaking this entire website.</p>
 		</div>
-		<a slot="link" class="font-bold" href="/projects/remake/"> Read about how I did it.</a>
+		<a slot="link" class="font-bold" href="/projects/remake/">Read the article.</a>
 	</Box>
-	<Box src={mathsImg} alt="maths playground, graph of squarewave">
+	<Box alt="maths playground, graph of squarewave" href="/projects/mathsplay/" src="https://images.unsplash.com/photo-1596495577886-d920f1fb7238?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80">
 		<div slot="desc">
-			<h2 class="font-bold">Maths and a UI</h2>
+			<h2 class="font-bold">Maths doesn't Share</h2>
 			<p>
 				If you've ever heard of wolframalpha or matlab, those inspired this project. It's like a
 				graphics calculator but for your mobile.
@@ -48,9 +45,9 @@
 		</div>
 		<a slot="link" class="font-bold" href="/projects/mathsplay/">View Project</a>
 	</Box>
-	<Box src={simonImg} alt="Simon Dev thumbnail">
+	<Box src={simonImg} alt="Simon Dev thumbnail" href="https://wonderful-stonebraker-0b161a.netlify.app/">
 		<div slot="desc">
-			<h2 class="font-bold">Simons World - The Remix</h2>
+			<h2 class="font-bold">Simons World</h2>
 			<p>
 				A 5 week sprint refactoring a huge proportion of simondev's original <a
 					href="https://github.com/simondevyoutube/Quick_3D_MMORPG">source code</a
@@ -62,14 +59,15 @@
 				>.
 			</p>
 		</div>
-		<a slot="link" class="font-bold" href="https://wonderful-stonebraker-0b161a.netlify.app/"
-			>View the Remix</a
-		>
-		<p class="text-xs">(Not recommended for mobile)</p>
+		<div slot="link">
+			<a class="font-bold" href="https://wonderful-stonebraker-0b161a.netlify.app/"
+			>View the Project</a>
+			<p class="text-xs">(Not built for mobile)</p>
+		</div>
 	</Box>
-	<Box src={threeSrc} alt="threejs logo">
+	<Box src={threeSrc} alt="threejs logo" href="https://github.com/mrdoob/three.js/pull/22510">
 		<div slot="desc">
-			<h2 class="font-bold">Now with more fetch!</h2>
+			<h2 class="font-bold">Go Fetch!</h2>
 			<p>
 				Another good effort to bring three.js up to speed with modern javascript. specifically by
 				implementing the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API"
@@ -79,12 +77,12 @@
 			</p>
 		</div>
 		<a slot="link" class="font-bold" href="https://github.com/mrdoob/three.js/pull/22510"
-			>Github PR</a
+			>Github Pull Request</a
 		>
 	</Box>
-	<Box src={threeSrc} alt="threejs logo">
+	<Box src={threeSrc} alt="threejs logo" href="https://github.com/mrdoob/three.js/issues/19986">
 		<div slot="desc">
-			<h2 class="font-bold">We're doing it with class</h2>
+			<h2 class="font-bold">Styling with Class</h2>
 			<p>
 				This was a substantial multi-year effort to bring three.js into the future with modern
 				javascript 🤵👏 I'm very pleased to have helped facilitate and be part of the movement.
@@ -94,9 +92,35 @@
 			>Github Issue</a
 		>
 	</Box>
-	<Box src={lavaCubeImg} alt="lava cube world">
+	<Box src={threeSrc} alt="threejs logo" href="/projects/parsingthreejs/">
 		<div slot="desc">
-			<h2 class="font-bold">The Floor is Lava!</h2>
+			<h2 class="font-bold">Javascript and Three.js</h2>
+			<p>One does not simply parse three.js 🤷‍♂️ Getting things wrong and keeping on going 🕵️‍♂️</p>
+		</div>
+		<a slot="link" class="font-bold" href="/projects/parsingthreejs/">Read the Article</a>
+	</Box>
+	<Box alt="university lecture theatre" href="/projects/lastslides/" src="https://images.unsplash.com/photo-1519452575417-564c1401ecc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80">
+		<div slot="desc">
+			<h2 class="font-bold">University Lessons</h2>
+			<p>
+				There'd be large scale studies showing the importance of education right?
+			</p>
+		</div>
+		<a slot="link" class="font-bold" href="/projects/lastslides/">Yeah, Nah.</a>
+	</Box>
+	<Box src={tugImg} alt="TUG concept art of a golem" href="/projects/storyaboutTUG/">
+		<div slot="desc">
+			<h2 class="font-bold">The Untitled Game</h2>
+			<p>
+				A highly ambitious 3D sandbox game, aiming at being a better minecraft. It was a huge
+				undertaking but fell apart in the end.
+			</p>
+		</div>
+		<a slot="link" class="font-bold" href="/projects/storyaboutTUG/">Read the Article</a>
+	</Box>
+	<Box src={lavaCubeImg} alt="lava cube world" href="/projects/lavafloor/">
+		<div slot="desc">
+			<h2 class="font-bold">Death by Lava!</h2>
 			<p>An age old game remade with three.js and cannon.js</p>
 		</div>
 		<div slot="link">
@@ -104,40 +128,4 @@
 			<p class="text-xs">(Not built for mobile)</p>
 		</div>
 	</Box>
-	<Box src={threeSrc} alt="threejs logo">
-		<div slot="desc">
-			<h2 class="font-bold">Parsing Three.js</h2>
-			<p>One does not simply parse three.js 🤷‍♂️ Getting things wrong and keeping on going 🕵️‍♂️</p>
-		</div>
-		<a slot="link" class="font-bold" href="/projects/parsingthreejs/">Read the write-up</a>
-	</Box>
-	<Box src={revealjsImg} alt="revealjs">
-		<div slot="desc">
-			<h2 class="font-bold">The Last Slides</h2>
-			<p>
-				When university lecture notes meet the web. Fair warning, tertairy material can be rather
-				dry and tedious.
-			</p>
-		</div>
-		<a slot="link" class="font-bold" href="/projects/lastslides/">View the slide deck</a>
-	</Box>
-	<Box src={tugImg} alt="TUG concept art of a golem">
-		<div slot="desc">
-			<h2 class="font-bold">TUG mod development</h2>
-			<p>
-				A highly ambitious 3D sandbox game, aiming at being a better minecraft. It was a huge
-				undertaking but fell apart in the end.
-			</p>
-		</div>
-		<a slot="link" class="font-bold" href="/projects/storyaboutTUG/">Read the Write-up</a>
-	</Box>
-	<!-- <Box></Box> -->
-	<!-- Images from unsplash
-		lava face - https://images.unsplash.com/photo-1634919872856-8da426d5540a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80
-		space man - https://images.unsplash.com/photo-1630152389714-46b8c393f555?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=70
-		mountain car light stream - https://images.unsplash.com/photo-1622960748104-094380e1d892?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=70
-		mountain snow - https://images.unsplash.com/photo-1638809264876-63bf0a05f4c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60
-		red texture - https://images.unsplash.com/photo-1590598016954-bc7ff4ad05d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8aVVJc25WdGpCMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60
-		nice unfocused lights - https://images.unsplash.com/photo-1638862457124-12c14e5fbd11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8aVVJc25WdGpCMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60
-		-->
 </ul>
