a10b780dbfe25107f2762d275c90721249a87fef
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-22 12:25:41 +1300

    break write-up's into sections with columns

--- src/routes/projects/remake.svelte
+++ src/routes/projects/remake.svelte
@@ -4,33 +4,35 @@
 
 <WriteUp>
 	<h1 class="text-center">The Remake</h1>
-	<p>Dec 2021</p>
-	<p>
-		This project started taking shape really quickly. <a href="https://kit.svelte.dev/"
-			>Svelte.kit</a
-		>
-		and <a href="https://tailwindcss.com/">Tailwindcss</a> have been the best development choices I've ever made.
-		After doing the usual configuration and deployment checks, it was time to integrate my
-		old projects.
-	</p>
-	<p>
-		Nowhere can anyone find better examples of how far they've come than by looking back at their own projects years later. To web developers out there,
-		some of my oldies used global scripts within the <code>index.html</code>... we've come a long way. 
-		The idea of mobile first and responsive image sizing would've been huge development efforts back then. Now I'm simply in shock
-		by how quickly I could get those same things done with less effort.
-	</p>
-	<p>
-		Remaking old projects was not a straight forward decision. Doing so expands the scope of the project which in turn takes more time. It's usually easier to start from stratch. When I looked into the <code>Reveal.js</code> project all I could see was
-		some clever css styling for 2013. Good in it's own time but not as good as tailwindcss today. For the interested reader, I suggest watching tailwind's <a
-			href="https://www.youtube.com/channel/UCOe-8z68tgw9ioqVvYM4ddQ">youtube videos</a
-		>. The kicker for me was that implementing the same features as <code>Reveal.js</code> is simpler with tailwind.
-	</p>
-	<p>
-		Svelte's also very deserving of the lime light. Snappy navigation within the website is the default. Anyone on the internet these days has moved on from a
-		website because it took too long to load or do something. Svelte.kit is the result of years of high requirements as well as a persmise of solving common issues that plague the well known frameworks.
-		It was a no brainer for me.
-	</p>
-	<p>
-		All in all, the remake only took me a couple of weeks including rewriting and rewording some articles. Darn good use of my time.
-	</p>
+	<section class="py-2 md:columns-2 lg:columns-3">
+		<p>Dec 2021</p>
+		<p>
+			This project started taking shape really quickly. <a href="https://kit.svelte.dev/"
+				>Svelte.kit</a
+			>
+			and <a href="https://tailwindcss.com/">Tailwindcss</a> have been the best development choices I've ever made.
+			After doing the usual configuration and deployment checks, it was time to integrate my
+			old projects.
+		</p>
+		<p>
+			Nowhere can anyone find better examples of how far they've come than by looking back at their own projects years later. To web developers out there,
+			some of my oldies used global scripts within the <code>index.html</code>... we've come a long way. 
+			The idea of mobile first and responsive image sizing would've been huge development efforts back then. Now I'm simply in shock
+			by how quickly I could get those same things done with less effort.
+		</p>
+		<p>
+			Remaking old projects was not a straight forward decision. Doing so expands the scope of the project which in turn takes more time. It's usually easier to start from stratch. When I looked into the <code>Reveal.js</code> project all I could see was
+			some clever css styling for 2013. Good in it's own time but not as good as tailwindcss today. For the interested reader, I suggest watching tailwind's <a
+				href="https://www.youtube.com/channel/UCOe-8z68tgw9ioqVvYM4ddQ">youtube videos</a
+			>. The kicker for me was that implementing the same features as <code>Reveal.js</code> is simpler with tailwind.
+		</p>
+		<p>
+			Svelte's also very deserving of the lime light. Snappy navigation within the website is the default. Anyone on the internet these days has moved on from a
+			website because it took too long to load or do something. Svelte.kit is the result of years of high requirements as well as a persmise of solving common issues that plague the well known frameworks.
+			It was a no brainer for me.
+		</p>
+		<p>
+			All in all, the remake only took me a couple of weeks including rewriting and rewording some articles. Darn good use of my time.
+		</p>
+	</section>
 </WriteUp>
