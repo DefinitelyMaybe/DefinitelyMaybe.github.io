cfe1b76e39575fd1dc66eaad56fdb11a72c6199e
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-22 16:16:02 +1300

    scroll and snap work

--- src/routes/projects/lastslides.svelte
+++ src/routes/projects/lastslides.svelte
@@ -8,16 +8,32 @@
 	<meta name="author" content="Aaron Dekker" />
 </svelte:head>
 
-<div class="snap-x flex stretch">
-	<Slide>
-		<p>hello world</p>
-	</Slide>
-	<!-- <Slide>
-		<p>Sir Ken Robinson <a href="https://www.youtube.com/watch?v=iG9CE55wbtY&t=1s">video</a> </p>
-	</Slide>
-	<Slide>
-		<p>Ron Finely & Simon Sinek <a href="https://youtu.be/eQ0A-4DhBeA">video</a></p>
-	</Slide> -->
+<div class="">
+	<div class="flex snap-x overflow-x-scroll snap-mandatory">
+		<Slide>
+			<div class="flex flex-col items-center text-center justify-center">
+				<p class="text-xs">to</p>
+				<h1>Sir Ken Robinson</h1>
+				<p>without whom we'd still be thinking we could standardize our way into the future</p>
+			</div>
+		</Slide>
+		<Slide>
+			<p>Crying is all right in its way while it lasts. But you have to stop sooner or later, and then you still have to decide what to do.</p>
+			<p>- C.S Lewis, The Silver Chair</p>
+		</Slide>
+		<Slide>
+			Lessons from my Ted talks
+		</Slide>
+		<Slide>
+			Lessons from tiktok
+		</Slide>
+		<!-- <Slide>
+			<p>Sir Ken Robinson <a href="https://www.youtube.com/watch?v=iG9CE55wbtY&t=1s">video</a> </p>
+		</Slide>
+		<Slide>
+			<p>Ron Finely & Simon Sinek <a href="https://youtu.be/eQ0A-4DhBeA">video</a></p>
+		</Slide> -->
+	</div>
 </div>
 
 <!-- <WriteUp>
