446525a97478ac3b4cb8ad82c25836c886745b22
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-24 11:53:57 +1300

    change to div

--- src/lib/Slide.svelte
+++ src/lib/Slide.svelte
@@ -1,8 +1,8 @@
 <script>
-  export let id
+  export let id = ""
 </script>
 
-<section id="{id}" class="flex flex-col justify-center snap-center snap-always min-w-full mx-8 p-4
+<div id="{id}" class="flex flex-col justify-center snap-center snap-always min-w-full mx-8 p-4
   shadow-lg dark:shadow-[#2d333b] dark:shadow-md dark:border-[1px] dark:border-[#2d333b]">
   <slot></slot>
-</section>
+</div>
