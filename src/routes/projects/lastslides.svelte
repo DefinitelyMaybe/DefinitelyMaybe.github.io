88fa9a2fcc545d0f424a15578c1525e48a4e6c52
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-12-23 18:50:42 +1300

    create hrefs to particular slides

--- src/routes/projects/lastslides.svelte
+++ src/routes/projects/lastslides.svelte
@@ -14,13 +14,30 @@
 		<h1 class="font-bold">Sir Ken Robinson</h1>
 		<p>without whom we'd still be thinking we could standardize our way into the future</p>
 	</Slide>
-	<Slide>
+	<Slide id="contents">
 		<h1>Contents:</h1>
 		<ol class="py-4">
-			<li>Predictability</li>
-			<li>Problems within education</li>
-			<li>Unresolvable Conflicts</li>
+			<li><a href="#predict">Predictability</a></li>
+			<li><a href="#education">Problems within education</a></li>
+			<li><a href="#conflicts">Unresolvable Conflicts</a></li>
 		</ol>
+	</Slide>
+	<Slide id="predict">
+		<h1>Predictability</h1>
+		<a href="contents">contents</a>
+	</Slide>
+	<Slide>
+		<p>As with many things, giving an initial low resolution picture or metaphor is best however because this is the end of 2021, we can go a bit deeper</p>
+		<p>Covid was and will still be a big deal going forward. Did anyone predict that before 2019? The answer is yes and no.</p>
+		<p>To my knowledge, Bill Gates has been worried about this sort of crisis for quite some time.</p>
+		<p>I'm not surprised that we've ended up in this situation.</p>
+	</Slide>
+	<Slide id="education">
+		<h1>Problems within education</h1>
+	</Slide>
+	<Slide></Slide>
+	<Slide id="conflicts">
+		<h1>Unresolvable Conflicts</h1>
 	</Slide>
 	<Slide>
 		<p>Crying is all right in its way while it lasts. But you have to stop sooner or later, and then you still have to decide what to do.</p>
