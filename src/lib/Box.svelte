be8b034725921ca1ebfc6bdd604861d48e9dca01
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-04-19 16:12:08 +1200

    adjust styles

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -9,9 +9,8 @@
 
 <div
 	on:click={goto(href)}
-	class="flex flex-col justify-start rounded-md border-[1px] pb-1 text-center shadow-lg
-	hover:cursor-pointer dark:border-[1px] dark:border-[#2d333b] dark:shadow-md
-		dark:shadow-[#2d333b]">
+	class="flex flex-col justify-start rounded-md border-[1px] pb-1 text-center shadow-lg hover:cursor-pointer
+		dark:border-[1px] dark:border-[#2d333b] dark:shadow-md	dark:shadow-[#2d333b]">
 	<img {src} {alt} class="w-full rounded-md" />
 	<div class="px-2 py-2">
 		<slot name="desc" />
