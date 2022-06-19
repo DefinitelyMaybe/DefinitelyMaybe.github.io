5b40587f2970077296066098c96eee1bbae4aad9
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-06-20 11:13:38 +1200

    add extras

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -5,12 +5,14 @@
 	/** @type {string} */
 	export let alt;
 	export let href;
+	export let additions = '';
+	// TODO-DefinitelyMaybe: Instead of literal tailwindcss additions, maybe make some classes like: big, medium, long, wide etc...
 </script>
 
 <div
 	on:click={goto(href)}
 	class="flex flex-col justify-start rounded-md border-[1px] pb-1 text-center shadow-lg hover:cursor-pointer
-		dark:border-[1px] dark:border-[#2d333b] dark:shadow-md	dark:shadow-[#2d333b]">
+		dark:border-[1px] dark:border-[#2d333b] dark:shadow-md	dark:shadow-[#2d333b] {additions}">
 	<img {src} {alt} class="w-full rounded-md" />
 	<div class="px-2 py-2">
 		<slot name="desc" />
