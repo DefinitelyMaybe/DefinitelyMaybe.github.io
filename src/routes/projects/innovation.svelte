1e32cf19f5b6987f15fe91185210323788080e21
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-06-22 16:42:08 +1200

    more writing about innovation

--- src/routes/projects/innovation.svelte
+++ src/routes/projects/innovation.svelte
@@ -26,19 +26,43 @@
 	<section class="py-2 md:columns-2 lg:columns-3">
 		<p>
 			We all know innovation is important, it's the guantlet by which all new ideas must pass before
-			becoming a reality. Which is a bit of a shame as most people figure out - making even the
-			smallest idea come to life requires so much upfront investment that mostly it's better not to
-			try. Just go to work, pick up that pay check and if you've got anything left, find something
-			to do that means something.
+			becoming a reality. It's a pretty big barrier to entry as most people figure out - making even
+			the smallest idea come to life requires a ton of investment, not just money but time, effort
+			and sacrifice. It can easily feel disheartening, like its better not to try, and this is
+			called the status quo.
 		</p>
 		<p>
-			Much like trying to innovate, sitting down for a cup of tea (or coffee) isn't going to get you
-			anywhere but is also the only way to get anywhere. I'll explain. Firstly, other people are a
-			multiplier propertional to how well you connect with each other. If you're running the
-			guantlet, there is not better aid than a friend who believes in you. Secondly, a cup of tea in
-			isolation isn't important. What's important is the cummulation of cup's of tea over time. For
-			the guantlet, someone standing with you the whole way is worth more than a thosand people
-			cheering you on during the highlights.
+			Peoples reaction to innovation can be roughly divided in two camps. Either they want it
+			because it has great potential or, they're afraid of the consequences of breaking a thing that
+			worked. Buffering something fagile from the outside world is a natural strategy as old as your
+			immune system. Your computer is also an example. It's layers, protocols and messaging
+			surrounding a fagile bit of electronics thats pretty darn easy to break.
+		</p>
+		<p>
+			War is an analogy usually coupled with innovation but your immune system is a better one. The
+			goal of your immune system is to balance your body's health - which the majority of the time
+			is a constant battle between itself and the outside world. Things can go awry, like in extreme
+			cases cancer and auto-immune diseases, but on the whole you'll probably live to a ripe old
+			age. That is success and innovation is a integral part. Virus's and Bacteria aren't sitting
+			back too disheartened by your defenses, no, you're going to get colds again and again.
+			Survival is dependant upon innovation.
+		</p>
+		<p>
+			Lowering the barriers.
+		</p>
+		<p>
+			Amoung the various kicker's of trying to innovate is that commincating the problem can be very
+			hard wheras an adequte solution could be something your kid comes up with. It's the source of
+			a good joke - depending on the day.
+		</p>
+		<p>
+			Much like trying to innovate, communicating your next idea with others usually doesn't get you
+			anywhere but is also the first worthy hurdle to overcome. other people are a multiplier
+			propertional to how well you connect with each other. If you're running the guantlet, there is
+			not better aid than a friend who believes in you. Secondly, a cup of tea in isolation isn't
+			important. What's important is the cummulation of cup's of tea over time. For the guantlet,
+			someone standing with you the whole way is worth more than a thosand people cheering you on
+			during the highlights.
 		</p>
 		<p>
 			<i
@@ -47,17 +71,7 @@
 					>Outliers</a
 				>. I thoroughly enjoyed it as an audio book]</i>
 		</p>
-		<p>
-			Lots of people run into these two issues. Either they want it because change has great
-			potential or, they're afraid of the consequences of the wrong change. It's a slow, painful,
-			costly experience which equates to flipping the innovation coin once. That potential gets
-			measured and the cost is paid along the way. Amoung the various kicker's of trying to innovate
-			is that commincating the problem can be very hard wheras an adequte solution could be
-			something your kid comes up with. It's the source of a good joke - depending on the day.
-		</p>
-		<p>
-			When and how does innovation happen anyway?
-		</p>
+		<p>When and how does innovation happen anyway?</p>
 		<p>Minecraft</p>
 		<p>Tiktok</p>
 		<p>IFC.js</p>
