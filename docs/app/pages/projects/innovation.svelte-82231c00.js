1c94e5a1614a4e72fbc02d9d01898c3d8f886d42
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-06-22 19:45:28 +1200

    update build

---
+++ docs/app/pages/projects/innovation.svelte-82231c00.js
@@ -0,0 +1,53 @@
+import{S as Bt,i as Ct,s as Mt,k as c,w as At,G as Ht,d as e,m as d,x as Ot,g as w,y as Ut,q as Vt,o as Gt,B as Nt,e as s,t as h,c as r,a as l,h as u,b as wt,H as t,E as zt}from"../../chunks/index-deee8842.js";import{t as Dt}from"../../chunks/Header-5af432b1.js";import{W as Jt}from"../../chunks/WriteUp-a37e0515.js";import"../../chunks/index-fe587174.js";function Kt(M){let p,m,g,n,y,v,U,I,V,G,$,N,A,k,z,H,o,P,D,J,x,K,Q,W,R,X,q,Z,tt,j,et,ot,S,nt,at,T,b,it,_,st,rt,lt,Y,ht,ut,F,ct,dt,L,pt,mt,B,yt,ft,C,gt;return{c(){p=s("p"),m=h("Structure will set you free"),g=c(),n=s("ul"),y=s("li"),v=h("Why"),U=c(),I=s("li"),V=h("How"),G=c(),$=s("li"),N=h("What"),A=c(),k=s("p"),z=h("..."),H=c(),o=s("section"),P=s("p"),D=h(`We all know innovation is important, it's the guantlet by which all new ideas must pass before\r
+			becoming a reality. It's a pretty big barrier to entry as most people figure out - making even\r
+			the smallest idea come to life requires a ton of investment, not just money but time, effort\r
+			and sacrifice. It can easily feel disheartening, like its better not to try, and this is\r
+			called the status quo.`),J=c(),x=s("p"),K=h(`Peoples reaction to innovation can be roughly divided in two camps. Either they want it\r
+			because it has great potential or, they're afraid of the consequences of breaking a thing that\r
+			worked. Buffering something fagile from the outside world is a natural strategy as old as your\r
+			immune system. Your computer is also an example. It's layers, protocols and messaging\r
+			surrounding a fagile bit of electronics thats pretty darn easy to break.`),Q=c(),W=s("p"),R=h(`War is an analogy usually coupled with innovation but your immune system is a better one. The\r
+			goal of your immune system is to balance your body's health - which the majority of the time\r
+			is a constant battle between itself and the outside world. Things can go awry, like in extreme\r
+			cases cancer and auto-immune diseases, but on the whole you'll probably live to a ripe old\r
+			age. That is success and innovation is a integral part. Virus's and Bacteria aren't sitting\r
+			back too disheartened by your defenses, no, you're going to get colds again and again.\r
+			Survival is dependant upon innovation.`),X=c(),q=s("p"),Z=h("Lowering the barriers."),tt=c(),j=s("p"),et=h(`Amoung the various kicker's of trying to innovate is that commincating the problem can be very\r
+			hard wheras an adequte solution could be something your kid comes up with. It's the source of\r
+			a good joke - depending on the day.`),ot=c(),S=s("p"),nt=h(`Much like trying to innovate, communicating your next idea with others usually doesn't get you\r
+			anywhere but is also the first worthy hurdle to overcome. other people are a multiplier\r
+			propertional to how well you connect with each other. If you're running the guantlet, there is\r
+			not better aid than a friend who believes in you. Secondly, a cup of tea in isolation isn't\r
+			important. What's important is the cummulation of cup's of tea over time. For the guantlet,\r
+			someone standing with you the whole way is worth more than a thosand people cheering you on\r
+			during the highlights.`),at=c(),T=s("p"),b=s("i"),it=h("[I will stress these two points by refering to a book about success called "),_=s("a"),st=h("Outliers"),rt=h(". I thoroughly enjoyed it as an audio book]"),lt=c(),Y=s("p"),ht=h("When and how does innovation happen anyway?"),ut=c(),F=s("p"),ct=h("Minecraft"),dt=c(),L=s("p"),pt=h("Tiktok"),mt=c(),B=s("p"),yt=h("IFC.js"),ft=c(),C=s("p"),gt=h(`the amount of investment you made in making it a thing in the first place. You want other\r
+			people, perspectives, ideas, methods, something, anything. You need innovation. You have a\r
+			complex question encapsulated within an environment who's details aren't making sense. For the\r
+			rest of us, well, the work I want to do with my pay check is build the something which enables\r
+			you to build/test/try-out your idea.`),this.h()},l(i){p=r(i,"P",{});var f=l(p);m=u(f,"Structure will set you free"),f.forEach(e),g=d(i),n=r(i,"UL",{class:!0});var E=l(n);y=r(E,"LI",{});var bt=l(y);v=u(bt,"Why"),bt.forEach(e),U=d(E),I=r(E,"LI",{});var vt=l(I);V=u(vt,"How"),vt.forEach(e),G=d(E),$=r(E,"LI",{});var kt=l($);N=u(kt,"What"),kt.forEach(e),E.forEach(e),A=d(i),k=r(i,"P",{});var _t=l(k);z=u(_t,"..."),_t.forEach(e),H=d(i),o=r(i,"SECTION",{class:!0});var a=l(o);P=r(a,"P",{});var Et=l(P);D=u(Et,`We all know innovation is important, it's the guantlet by which all new ideas must pass before\r
+			becoming a reality. It's a pretty big barrier to entry as most people figure out - making even\r
+			the smallest idea come to life requires a ton of investment, not just money but time, effort\r
+			and sacrifice. It can easily feel disheartening, like its better not to try, and this is\r
+			called the status quo.`),Et.forEach(e),J=d(a),x=r(a,"P",{});var It=l(x);K=u(It,`Peoples reaction to innovation can be roughly divided in two camps. Either they want it\r
+			because it has great potential or, they're afraid of the consequences of breaking a thing that\r
+			worked. Buffering something fagile from the outside world is a natural strategy as old as your\r
+			immune system. Your computer is also an example. It's layers, protocols and messaging\r
+			surrounding a fagile bit of electronics thats pretty darn easy to break.`),It.forEach(e),Q=d(a),W=r(a,"P",{});var $t=l(W);R=u($t,`War is an analogy usually coupled with innovation but your immune system is a better one. The\r
+			goal of your immune system is to balance your body's health - which the majority of the time\r
+			is a constant battle between itself and the outside world. Things can go awry, like in extreme\r
+			cases cancer and auto-immune diseases, but on the whole you'll probably live to a ripe old\r
+			age. That is success and innovation is a integral part. Virus's and Bacteria aren't sitting\r
+			back too disheartened by your defenses, no, you're going to get colds again and again.\r
+			Survival is dependant upon innovation.`),$t.forEach(e),X=d(a),q=r(a,"P",{});var Pt=l(q);Z=u(Pt,"Lowering the barriers."),Pt.forEach(e),tt=d(a),j=r(a,"P",{});var xt=l(j);et=u(xt,`Amoung the various kicker's of trying to innovate is that commincating the problem can be very\r
+			hard wheras an adequte solution could be something your kid comes up with. It's the source of\r
+			a good joke - depending on the day.`),xt.forEach(e),ot=d(a),S=r(a,"P",{});var Wt=l(S);nt=u(Wt,`Much like trying to innovate, communicating your next idea with others usually doesn't get you\r
+			anywhere but is also the first worthy hurdle to overcome. other people are a multiplier\r
+			propertional to how well you connect with each other. If you're running the guantlet, there is\r
+			not better aid than a friend who believes in you. Secondly, a cup of tea in isolation isn't\r
+			important. What's important is the cummulation of cup's of tea over time. For the guantlet,\r
+			someone standing with you the whole way is worth more than a thosand people cheering you on\r
+			during the highlights.`),Wt.forEach(e),at=d(a),T=r(a,"P",{});var qt=l(T);b=r(qt,"I",{});var O=l(b);it=u(O,"[I will stress these two points by refering to a book about success called "),_=r(O,"A",{href:!0});var jt=l(_);st=u(jt,"Outliers"),jt.forEach(e),rt=u(O,". I thoroughly enjoyed it as an audio book]"),O.forEach(e),qt.forEach(e),lt=d(a),Y=r(a,"P",{});var St=l(Y);ht=u(St,"When and how does innovation happen anyway?"),St.forEach(e),ut=d(a),F=r(a,"P",{});var Tt=l(F);ct=u(Tt,"Minecraft"),Tt.forEach(e),dt=d(a),L=r(a,"P",{});var Yt=l(L);pt=u(Yt,"Tiktok"),Yt.forEach(e),mt=d(a),B=r(a,"P",{});var Ft=l(B);yt=u(Ft,"IFC.js"),Ft.forEach(e),ft=d(a),C=r(a,"P",{});var Lt=l(C);gt=u(Lt,`the amount of investment you made in making it a thing in the first place. You want other\r
+			people, perspectives, ideas, methods, something, anything. You need innovation. You have a\r
+			complex question encapsulated within an environment who's details aren't making sense. For the\r
+			rest of us, well, the work I want to do with my pay check is build the something which enables\r
+			you to build/test/try-out your idea.`),Lt.forEach(e),a.forEach(e),this.h()},h(){wt(n,"class","list-inside list-disc text-gray-400"),wt(_,"href","https://www.gladwellbooks.com/titles/malcolm-gladwell/outliers/9780316040341/"),wt(o,"class","py-2 md:columns-2 lg:columns-3")},m(i,f){w(i,p,f),t(p,m),w(i,g,f),w(i,n,f),t(n,y),t(y,v),t(n,U),t(n,I),t(I,V),t(n,G),t(n,$),t($,N),w(i,A,f),w(i,k,f),t(k,z),w(i,H,f),w(i,o,f),t(o,P),t(P,D),t(o,J),t(o,x),t(x,K),t(o,Q),t(o,W),t(W,R),t(o,X),t(o,q),t(q,Z),t(o,tt),t(o,j),t(j,et),t(o,ot),t(o,S),t(S,nt),t(o,at),t(o,T),t(T,b),t(b,it),t(b,_),t(_,st),t(b,rt),t(o,lt),t(o,Y),t(Y,ht),t(o,ut),t(o,F),t(F,ct),t(o,dt),t(o,L),t(L,pt),t(o,mt),t(o,B),t(B,yt),t(o,ft),t(o,C),t(C,gt)},p:zt,d(i){i&&e(p),i&&e(g),i&&e(n),i&&e(A),i&&e(k),i&&e(H),i&&e(o)}}}function Qt(M){let p,m,g;return m=new Jt({props:{$$slots:{default:[Kt]},$$scope:{ctx:M}}}),{c(){p=c(),At(m.$$.fragment),this.h()},l(n){Ht('[data-svelte="svelte-yyu1mj"]',document.head).forEach(e),p=d(n),Ot(m.$$.fragment,n),this.h()},h(){document.title="Innovation"},m(n,y){w(n,p,y),Ut(m,n,y),g=!0},p(n,[y]){const v={};y&1&&(v.$$scope={dirty:y,ctx:n}),m.$set(v)},i(n){g||(Vt(m.$$.fragment,n),g=!0)},o(n){Gt(m.$$.fragment,n),g=!1},d(n){n&&e(p),Nt(m,n)}}}function Rt(M){return Dt.subscribe(p=>{}),[]}class oe extends Bt{constructor(p){super(),Ct(this,p,Rt,Qt,Mt,{})}}export{oe as default};
