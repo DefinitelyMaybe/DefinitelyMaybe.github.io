ca78bd3c6c5ab451eb83db24b1921c6621a7fc90
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-07 13:44:30 +1200

    fleshing it out more

--- src/routes/projects/TypedArrays.svelte
+++ src/routes/projects/TypedArrays.svelte
@@ -1,9 +1,22 @@
 <script>
- let userArray = ["🥝", "🍓", "🍍", "🍋", "🍑", "🍌", "🥑"]
+	// https://emojipedia.org/search/?q=fruit
+	let userArray = ['🥝', '🍓', '🍍', '🍋', '🍑', '🍌', '🥑'];
+	let typedArray;
 </script>
 
-<div class="flex flex-col grow bg-base-300">
-  <div>Numbers</div>
-  <div>Arrays</div>
-  <div>[{userArray}]</div>
+<div class="flex grow flex-col bg-base-300">
+	<div>Numbers</div>
+	<div>Arrays</div>
+  <div class="columns-2">
+    <div>The Buffer</div>
+    <div>The View</div>
+  </div>
+	<select class="input select">
+    <option default value="0">Uint8Array</option>
+    <option value="1">Uint16Array</option>
+    <option value="2">Uint32Array</option>
+    <option value="3">Uint64Array</option>
+  </select>
+	<div>[{userArray}]</div>
+  <div>How to index 2 diminsional arrays: index * itemSize + offset</div>
 </div>
