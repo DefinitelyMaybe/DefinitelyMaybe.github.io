e64367ce94f6c9bad68a39f66d95dda34e54fa00
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-07 15:15:43 +1200

    hello simplex terrain, my old friend

--- src/lib/terrain/Terrain.svelte
+++ src/lib/terrain/Terrain.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { Mesh } from 'threlte';
-	import { PlaneBufferGeometry, DoubleSide, MeshStandardMaterial, BufferAttribute } from 'three';
+	import { PlaneBufferGeometry, DoubleSide, MeshStandardMaterial, BufferAttribute, Float32BufferAttribute } from 'three';
 	import simplex from 'simplex-noise';
 
 	export let seed = 123456789;
@@ -14,25 +14,25 @@
 	export let planeWidth = 100;
 	export let planeheight = 100;
 
-	const bytesInFloat32 = 4;
+	let noise, geometry;
 
-	let noise;
-	let positionsArray;
-	let geometry = new PlaneBufferGeometry(planeWidth, planeheight, planeWidth, planeheight);
+	$: {
+		noise = new simplex(seed);
+		geometry = new PlaneBufferGeometry(planeWidth, planeheight, planeWidth, planeheight);
+		const vertices = geometry.attributes.position.array
+		for ( let i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
+			vertices[ j + 2 ] = calc2DNoise(vertices[j], vertices[j+1]);
+		}
+		geometry.computeVertexNormals()
+		// for (let x = 0; x < planeWidth; x++) {
+		// 	for (let z = 0; z < planeheight; z++) {
+		// 		vertices.push(x, calc2DNoise(x, z), z)
+		// 	}
+		// }
+	  // geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
+	}
 
-	// $: {
-	// 	noise = new simplex(seed);
-	//   positionsArray = new Float32Array(bytesInFloat32*planeWidth*planeheight)
-	// 	for (let x = 0; i < planeWidth; i++) {
-	// 		for (let z = 0; j < planeheight; j++) {
-	// 			const noiseValue = get(x, z);
-	//       positionsArray.set([x, noiseValue, z], 4*x + z) //.push(x, noiseValue, z)
-	// 		}
-	// 	}
-	//   geometry.setAttribute('position', new BufferAttribute(positionsArray, 3));
-	// }
-
-	function get(x, y) {
+	function calc2DNoise(x, y) {
 		const G = Math.pow(2, -persistence);
 		const xs = x / scale;
 		const ys = y / scale;
@@ -61,4 +61,4 @@
 	receiveShadow
 	rotation={{ x: -90 * (Math.PI / 180) }}
 	{geometry}
-	material={new MeshStandardMaterial({ side: DoubleSide, color: 'white' })} />
+	material={new MeshStandardMaterial({ side: DoubleSide, color: 'grey' })} />
