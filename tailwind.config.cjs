967e3d241da8deffd872f36443e004a9a7f699ae
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-09 13:52:13 +1200

    add personal theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,28 @@
 module.exports = {
-	content: ['./src/**/*.{svelte,css,html}'],
-	darkMode: 'class',
-	variants: {
-		extend: {}
-	},
-	plugins: [
-		require('@tailwindcss/forms'), require('daisyui')
-	]
+  content: ["./src/**/*.{svelte,css,html}"],
+  daisyui: {
+    themes: [
+      {
+        maybetheme: {
+          "primary": "#3b82f6",
+          "secondary": "#38bdf8",
+          "accent": "#bfdbfe",
+          "neutral": "#1c1917",
+          "base-100": "#4b5563",
+          "info": "#38bdf8",
+          "success": "#65a30d",
+          "warning": "#f59e0b",
+          "error": "#dc2626",
+        },
+      },
+    ],
+  },
+  darkMode: "class",
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
 };
