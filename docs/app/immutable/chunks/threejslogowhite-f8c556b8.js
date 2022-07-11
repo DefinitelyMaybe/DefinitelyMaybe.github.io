4a15ab6ae91933874520455bbc3fca48b6af7f8b
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-12 10:34:39 +1200

    update build

--- docs/app/immutable/chunks/threejslogowhite-f8c556b8.js
+++
@@ -1,1 +0,0 @@
-var e="/app/immutable/assets/threejslogodark-91e31d49.png",a="/app/immutable/assets/threejslogowhite-39196387.png";export{e as a,a as t};
---
+++ docs/app/immutable/pages/projects/storyaboutTUG.svelte-998bd6fb.js
@@ -0,0 +1,77 @@
+import{S as le,i as he,s as ce,k as i,e as o,t as h,L as de,d as a,m as s,c as n,a as l,h as c,b as u,I as et,g as m,J as e,E as jt}from"../../chunks/index-269e7057.js";import{g as me}from"../../chunks/TUGconceptArtmini-e50cbb6b.js";var ue="/app/immutable/assets/TUGupdate-5f418ad7.jpg",pe="/app/immutable/assets/TUGupdatedart-28509285.jpg",fe="/app/immutable/assets/TUGmount-1bc56aa8.gif";function ge(se){let T,I,at,R,r,P,ot,nt,U,rt,it,A,st,lt,W,ht,ct,S,dt,mt,D,ut,pt,G,ft,gt,N,bt,wt,H,yt,z,_,qt,B,f,j,vt,It,q,_t,kt,K,Tt,Gt,L,Mt,Et,O,xt,X,k,Kt,Y,g,Pt,M,Ut,At,E,Wt,St,Q,C,V,w,F,Dt,Nt,J,Ht,Z,y,Lt,$,v,Ot;return{c(){T=i(),I=o("h1"),at=h("Introducing TUG"),R=i(),r=o("section"),P=o("p"),ot=h("August 2016"),nt=i(),U=o("h3"),rt=h("Where to begin?"),it=i(),A=o("p"),st=h(`We'll approach T.U.G. by circling around at the distance, gaining perspective as we go and then
+		dive on in.`),lt=i(),W=o("p"),ht=h(`Right, but where to begin? Probably with Minecraft. For the uninitiated, Minecraft is a
+		procedurally generated block game. Procedural means that aspects of the game are not strictly
+		pre-defined. The classic example is terrain. When the player starts a game there is not a
+		pre-made map designed from head to toe. Instead, an algorithm executes a series of instructions
+		building up a random map layer by layer.`),ct=i(),S=o("p"),dt=h(`The next unique thing about Minecraft is that there is no story, no narrator, no objective, no
+		tutorial - nothing. Naturally this stumps a lot of people. Why would people play a game that has
+		no point to it? I could try to give a reason to this but instead let's point out a fun fact on
+		sales: Minecraft has sold a total of 24 million copies since it's release in 2009, 10 million
+		more than it's closest runner up - World of Warcraft.`),mt=i(),D=o("p"),ut=h(`Now, onto the most notable thing about Minecraft - the blocks. How did that become a thing?
+		Simply put, this is because behind the scenes terrain is explicitly coupled with grid
+		coordinates. Hugely easier to implement in code but does restrict terrain aesthetics. It should
+		be noted that even with the restriction, Minecraft\u2019s community has built tons on-top of and
+		around the game to the point where one would call it a feature rather than anything else.`),pt=i(),G=o("h3"),ft=h("What's TUG like at the moment?"),gt=i(),N=o("p"),bt=h(`It\u2019s a sandbox like minecraft. You can explore an infinite world, craft weapons, tools and
+		armour, build castles, hunt down animals and more. I\u2019m a huge fan of the subtleties that come
+		with the voxels - super nice curves in terrain when digging out caves and of course the 3D
+		modeled objects populating that terrain are a big plus.`),wt=i(),H=o("p"),yt=h(`Game development is like a long roller coaster ride and Nerd Kingdom has had it\u2019s fair share of
+		ups and downs. In brief, TUG was at an early stage of development. Nerd Kingdom is preparing to
+		release version 2 around the end of 2016. With it will be a lessening of the hardware
+		restrictions due to OpenGL, the introduction of Javascript for scripting, node.js integration, a
+		new art style and much more.`),z=i(),_=o("img"),B=i(),f=o("section"),j=o("h3"),vt=h("Where do I come into the story?"),It=i(),q=o("p"),_t=h(`Well, I had somehow managed to bump into NK\u2019s Kickstarter campaign back in 2013. It caught my
+		eye for a number of reasons among which was their simple approach to \u201Cwe want to be bigger and
+		better than Minecraft\u201D. In plain, start where Minecraft should have - with the community and
+		grow with them.`),kt=i(),K=o("p"),Tt=h(`I was a star grazer for at least a year. See, DirectX11 was a hardware requirement of the first
+		version. I built myself a computer when I finally had the opportunity (#PoorStudent). After a
+		while of playing the game normally I decided to give modding a go. I had heard of modders before
+		but it had always been a black-box. Luckily for me, there were other members within the
+		community for me to engage with and learn from.`),Gt=i(),L=o("p"),Mt=h(`My Mods that I\u2019ve put the most work into would be Portal Stones and Dark Ages. Both mods had
+		elements of 3D model manipulation, GIMP image editing, scripting and MATHS! Forgive me but I\u2019m
+		the kind of person who gets super excited the intersections between maths and computer science.
+		Portal Stones was all about quaternions - how to rotate objects in 3D space and Dark Ages was
+		all about simplex noise - how to procedurally generate terrain.`),Et=i(),O=o("p"),xt=h(`There is more to come for the future of TUG, namely neural networks/AI however that story is one
+		that we should not speculate on. As a small teaser, here\u2019s a render of the art direction for TUG
+		v2.`),X=i(),k=o("img"),Y=i(),g=o("p"),Pt=h("If you're interest here is a link to one of the "),M=o("a"),Ut=h("original videos talking about TUG"),At=h(`
+	and as well as a mod that I developed for the game called
+	`),E=o("a"),Wt=h("DarkAges"),St=h("."),Q=i(),C=o("hr"),V=i(),w=o("section"),F=o("p"),Dt=h("December 2021"),Nt=i(),J=o("p"),Ht=h(`This is one of those somber moments looking back on a game I believed in. Now TUG is nothing.
+		Apparantly there was a lot of tension between @ino and another director within the company. @ino
+		stepped down, communication with the commuinity was cut and nobody's heard anything since. The
+		communitty deserved better than this and the game itself was heading in the right direction.`),Z=i(),y=o("img"),$=i(),v=o("img"),this.h()},l(t){de('[data-svelte="svelte-9p4hp"]',document.head).forEach(a),T=s(t),I=n(t,"H1",{class:!0});var Ct=l(I);at=c(Ct,"Introducing TUG"),Ct.forEach(a),R=s(t),r=n(t,"SECTION",{class:!0});var p=l(r);P=n(p,"P",{});var Ft=l(P);ot=c(Ft,"August 2016"),Ft.forEach(a),nt=s(p),U=n(p,"H3",{});var Jt=l(U);rt=c(Jt,"Where to begin?"),Jt.forEach(a),it=s(p),A=n(p,"P",{});var Rt=l(A);st=c(Rt,`We'll approach T.U.G. by circling around at the distance, gaining perspective as we go and then
+		dive on in.`),Rt.forEach(a),lt=s(p),W=n(p,"P",{});var zt=l(W);ht=c(zt,`Right, but where to begin? Probably with Minecraft. For the uninitiated, Minecraft is a
+		procedurally generated block game. Procedural means that aspects of the game are not strictly
+		pre-defined. The classic example is terrain. When the player starts a game there is not a
+		pre-made map designed from head to toe. Instead, an algorithm executes a series of instructions
+		building up a random map layer by layer.`),zt.forEach(a),ct=s(p),S=n(p,"P",{});var Bt=l(S);dt=c(Bt,`The next unique thing about Minecraft is that there is no story, no narrator, no objective, no
+		tutorial - nothing. Naturally this stumps a lot of people. Why would people play a game that has
+		no point to it? I could try to give a reason to this but instead let's point out a fun fact on
+		sales: Minecraft has sold a total of 24 million copies since it's release in 2009, 10 million
+		more than it's closest runner up - World of Warcraft.`),Bt.forEach(a),mt=s(p),D=n(p,"P",{});var Xt=l(D);ut=c(Xt,`Now, onto the most notable thing about Minecraft - the blocks. How did that become a thing?
+		Simply put, this is because behind the scenes terrain is explicitly coupled with grid
+		coordinates. Hugely easier to implement in code but does restrict terrain aesthetics. It should
+		be noted that even with the restriction, Minecraft\u2019s community has built tons on-top of and
+		around the game to the point where one would call it a feature rather than anything else.`),Xt.forEach(a),pt=s(p),G=n(p,"H3",{class:!0});var Yt=l(G);ft=c(Yt,"What's TUG like at the moment?"),Yt.forEach(a),gt=s(p),N=n(p,"P",{});var Qt=l(N);bt=c(Qt,`It\u2019s a sandbox like minecraft. You can explore an infinite world, craft weapons, tools and
+		armour, build castles, hunt down animals and more. I\u2019m a huge fan of the subtleties that come
+		with the voxels - super nice curves in terrain when digging out caves and of course the 3D
+		modeled objects populating that terrain are a big plus.`),Qt.forEach(a),wt=s(p),H=n(p,"P",{});var Vt=l(H);yt=c(Vt,`Game development is like a long roller coaster ride and Nerd Kingdom has had it\u2019s fair share of
+		ups and downs. In brief, TUG was at an early stage of development. Nerd Kingdom is preparing to
+		release version 2 around the end of 2016. With it will be a lessening of the hardware
+		restrictions due to OpenGL, the introduction of Javascript for scripting, node.js integration, a
+		new art style and much more.`),Vt.forEach(a),p.forEach(a),z=s(t),_=n(t,"IMG",{src:!0,alt:!0}),B=s(t),f=n(t,"SECTION",{class:!0});var b=l(f);j=n(b,"H3",{});var Zt=l(j);vt=c(Zt,"Where do I come into the story?"),Zt.forEach(a),It=s(b),q=n(b,"P",{});var $t=l(q);_t=c($t,`Well, I had somehow managed to bump into NK\u2019s Kickstarter campaign back in 2013. It caught my
+		eye for a number of reasons among which was their simple approach to \u201Cwe want to be bigger and
+		better than Minecraft\u201D. In plain, start where Minecraft should have - with the community and
+		grow with them.`),$t.forEach(a),kt=s(b),K=n(b,"P",{});var te=l(K);Tt=c(te,`I was a star grazer for at least a year. See, DirectX11 was a hardware requirement of the first
+		version. I built myself a computer when I finally had the opportunity (#PoorStudent). After a
+		while of playing the game normally I decided to give modding a go. I had heard of modders before
+		but it had always been a black-box. Luckily for me, there were other members within the
+		community for me to engage with and learn from.`),te.forEach(a),Gt=s(b),L=n(b,"P",{});var ee=l(L);Mt=c(ee,`My Mods that I\u2019ve put the most work into would be Portal Stones and Dark Ages. Both mods had
+		elements of 3D model manipulation, GIMP image editing, scripting and MATHS! Forgive me but I\u2019m
+		the kind of person who gets super excited the intersections between maths and computer science.
+		Portal Stones was all about quaternions - how to rotate objects in 3D space and Dark Ages was
+		all about simplex noise - how to procedurally generate terrain.`),ee.forEach(a),Et=s(b),O=n(b,"P",{});var ae=l(O);xt=c(ae,`There is more to come for the future of TUG, namely neural networks/AI however that story is one
+		that we should not speculate on. As a small teaser, here\u2019s a render of the art direction for TUG
+		v2.`),ae.forEach(a),b.forEach(a),X=s(t),k=n(t,"IMG",{src:!0,alt:!0}),Y=s(t),g=n(t,"P",{class:!0});var x=l(g);Pt=c(x,"If you're interest here is a link to one of the "),M=n(x,"A",{href:!0});var oe=l(M);Ut=c(oe,"original videos talking about TUG"),oe.forEach(a),At=c(x,`
+	and as well as a mod that I developed for the game called
+	`),E=n(x,"A",{href:!0});var ne=l(E);Wt=c(ne,"DarkAges"),ne.forEach(a),St=c(x,"."),x.forEach(a),Q=s(t),C=n(t,"HR",{class:!0}),V=s(t),w=n(t,"SECTION",{class:!0});var tt=l(w);F=n(tt,"P",{});var re=l(F);Dt=c(re,"December 2021"),re.forEach(a),Nt=s(tt),J=n(tt,"P",{});var ie=l(J);Ht=c(ie,`This is one of those somber moments looking back on a game I believed in. Now TUG is nothing.
+		Apparantly there was a lot of tension between @ino and another director within the company. @ino
+		stepped down, communication with the commuinity was cut and nobody's heard anything since. The
+		communitty deserved better than this and the game itself was heading in the right direction.`),ie.forEach(a),tt.forEach(a),Z=s(t),y=n(t,"IMG",{class:!0,src:!0,alt:!0}),$=s(t),v=n(t,"IMG",{class:!0,src:!0,alt:!0}),this.h()},h(){document.title="A Story about TUG",u(I,"class","text-center"),u(G,"class","lg:break-before-column"),u(r,"class","py-2"),et(_.src,qt=me)||u(_,"src",qt),u(_,"alt","TUG concept art of a golem"),u(f,"class","py-2 md:columns-2 lg:columns-3"),et(k.src,Kt=ue)||u(k,"src",Kt),u(k,"alt","The feet of an TUG avatar surrounding by seedlings"),u(M,"href","https://cdn.akamai.steamstatic.com/steam/apps/256657525/movie480.webm?t=1447378587"),u(E,"href","https://github.com/DefinitelyMaybe/DarkAges"),u(g,"class","py-4"),u(C,"class","py-2"),u(w,"class","py-2 md:columns-2 lg:columns-3"),u(y,"class","py-2"),et(y.src,Lt=pe)||u(y,"src",Lt),u(y,"alt","TUG updated artwork style"),u(v,"class","py-2"),et(v.src,Ot=fe)||u(v,"src",Ot),u(v,"alt","TUG mount artwork")},m(t,d){m(t,T,d),m(t,I,d),e(I,at),m(t,R,d),m(t,r,d),e(r,P),e(P,ot),e(r,nt),e(r,U),e(U,rt),e(r,it),e(r,A),e(A,st),e(r,lt),e(r,W),e(W,ht),e(r,ct),e(r,S),e(S,dt),e(r,mt),e(r,D),e(D,ut),e(r,pt),e(r,G),e(G,ft),e(r,gt),e(r,N),e(N,bt),e(r,wt),e(r,H),e(H,yt),m(t,z,d),m(t,_,d),m(t,B,d),m(t,f,d),e(f,j),e(j,vt),e(f,It),e(f,q),e(q,_t),e(f,kt),e(f,K),e(K,Tt),e(f,Gt),e(f,L),e(L,Mt),e(f,Et),e(f,O),e(O,xt),m(t,X,d),m(t,k,d),m(t,Y,d),m(t,g,d),e(g,Pt),e(g,M),e(M,Ut),e(g,At),e(g,E),e(E,Wt),e(g,St),m(t,Q,d),m(t,C,d),m(t,V,d),m(t,w,d),e(w,F),e(F,Dt),e(w,Nt),e(w,J),e(J,Ht),m(t,Z,d),m(t,y,d),m(t,$,d),m(t,v,d)},p:jt,i:jt,o:jt,d(t){t&&a(T),t&&a(I),t&&a(R),t&&a(r),t&&a(z),t&&a(_),t&&a(B),t&&a(f),t&&a(X),t&&a(k),t&&a(Y),t&&a(g),t&&a(Q),t&&a(C),t&&a(V),t&&a(w),t&&a(Z),t&&a(y),t&&a($),t&&a(v)}}}class ye extends le{constructor(T){super(),he(this,T,null,ge,ce,{})}}export{ye as default};
